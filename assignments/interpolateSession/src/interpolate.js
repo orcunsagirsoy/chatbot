a534c783e889ba6a33e738fae174a856d710d0f8
Orcun <orcun@mindbehind.com>
2022-03-30 00:45:28 +0300

    add regex solution

--- assignments/interpolateSession/src/interpolate.js
+++ assignments/interpolateSession/src/interpolate.js
@@ -1,5 +1,20 @@
 const interpolate = (value, session = {}, options = {}) => {
-    //TODO
+  let pattern = "";
+  if (Object.keys(session).length === 0) {
+    pattern = ".*?";
+  } else {
+    pattern = Object.keys(session).join("|");
+  }
+
+  return value.replace(
+    new RegExp(
+      options.leftDelimiter + "(" + pattern + ")" + options.rightDelimiter,
+      "g"
+    ),
+    function (m, c) {
+      return session[c] ? session[c] : "";
+    }
+  );
 };
 
-module.exports = { interpolate }
+module.exports = { interpolate };
